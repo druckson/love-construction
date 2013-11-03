5729f521f3bd31ee0a25eda929071893f8c34249
Dan Erickson <druckson@gmail.com>
2013-11-02 18:18:32 -0700

    switch to new messaging system

--- systems/construction.lua
+++ systems/construction.lua
@@ -10,17 +10,43 @@ local Construction = Class{
 }
 
 function Construction:setup(engine)
+    self.engine = engine
     local construction = self
-    engine.registry:register("init_entity", function(...)
-        construction:init_entity(...)
+    engine.messaging:register("begin_contact", function(a, b, collision)
+        local entity1 = a:getUserData()
+        local entity2 = b:getUserData()
+        if entity1.construction and entity2.construction and
+            not entity1.construction.connected and not entity2.construction.connected then
+            engine.messaging:emit("connect", entity1, entity2)
+        end
+        return true
     end)
-    engine.registry:register("remove_entity", function(...)
-        construction:remove_entity(...)
+
+    engine.messaging:register("end_contact", function(a, b, collision)
+        --local entity1 = a:getUserData()
+        --local entity2 = b:getUserData()
+        --if entity1.construction and entity2.construction and
+        --    entity1.construction.connected and entity2.construction.connected then
+        --    engine.messaging:emit("disconnect", entity1, entity2)
+        --end
+        --return true
+    end)
+
+    engine.messaging:register("connect", function(entity1, entity2)
+        print("Connect")
+        construction:connect(entity1, entity2)
+    end)
+
+    engine.messaging:register("disconnect", function(entity1, entity2)
+        entity1.construction.connected = false
+        entity2.construction.connected = false
     end)
 end
 
 function Construction:init_entity(entity, object)
-    entity.construction = {}
+    entity.construction = {
+        connected = false 
+    }
     table.insert(self.entities, entity)
 end
 
@@ -34,8 +60,10 @@ function Construction:remove_entity(entity)
 end
 
 function Construction:connect(o1, o2)
-    assert(o1.construction ~= nil and o2.construction ~= nil,
+    assert(o1.construction and o2.construction,
            "Connect: wrong argument types (construction expected)")
+
+    print(o1.transform, o2.transform)
 
     local locator = entity.new()
 
@@ -52,6 +80,20 @@ function Construction:connect(o1, o2)
 
     -- Remove the temporary locator
     o2BaseAncestor:removeParent()
+
+    o1.construction.connected = true
+    o2.construction.connected = true
+
+    print("Connecting...")
+
+    o2BaseAncestor.entity.physics.body:setPosition(o2BaseAncestor.entity.transform:getAbsolutePosition():unpack())
+    o2BaseAncestor.entity.physics.body:setAngle(o2BaseAncestor.entity.transform:getAbsoluteRotation())
+
+    local o1BaseAncestor = o1.transform:getBaseAncestor()
+    --o1BaseAncestor.entity.transform:setParent(locator.transform)
+
+    self.engine.systems['physics']:disable_physics(o1BaseAncestor.entity)
+    self.engine.systems['physics']:disable_physics(o2BaseAncestor.entity)
 
     return locator
 end
