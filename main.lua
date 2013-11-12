2a60db8df66d32a8d0a28cd0ef7d584d4c33666a
Dan Erickson <druckson@gmail.com>
2013-11-12 10:19:46 -0800

    add new systems to game

--- main.lua
+++ main.lua
@@ -13,24 +13,28 @@ local worldSize = vector.new(1000, 1000)
 local screenSize = vector.new(mode.width, mode.height)
 
 local engine = Engine()
-local physics = systems.Physics(integrators.RK4)
+local physics = systems.Physics(integrators.Euler, 5)
 local display = systems.Display()
 local player = systems.Player(display)
 local construction = systems.Construction()
 local electricity = systems.Electricity()
 local light = systems.Light()
+local generator = systems.Generator()
+local thruster = systems.Thruster()
 
 engine:addSystem("physics",      physics)
-engine:addSystem("display",      display)
 engine:addSystem("player",       player)
+engine:addSystem("display",      display)
 engine:addSystem("construction", construction)
 engine:addSystem("electricity",  electricity)
 engine:addSystem("light",        light)
+engine:addSystem("generator",    generator)
+engine:addSystem("thruster",     thruster)
 
 function love.load()
     ProFi:start()
     display:setScreenSize(screenSize.x, screenSize.y)
-    local scene = scenes.Scene2(engine)
+    local scene = scenes.Scene3(engine)
 end
 
 function love.keypressed(k)
@@ -42,14 +46,10 @@ function love.keypressed(k)
 end
 
 function love.update(dt)
-    dt = math.min(dt, 0.5)
-    --physics:update(dt)
-    --player:update(dt)
     engine:update(dt)
     engine.messaging:flush()
 end
 
 function love.draw()
     display:display()
-    --engine:display()
 end
