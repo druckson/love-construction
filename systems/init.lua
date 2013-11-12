e78eb18948663cfcd18da0b3f67b73af720b2dca
Dan Erickson <druckson@gmail.com>
2013-11-12 10:44:17 -0800

    add new systems

--- systems/init.lua
+++ systems/init.lua
@@ -4,5 +4,7 @@ return {
     Physics = require "systems/physics",
     Construction = require "systems/construction",
     Electricity = require "systems/electricity",
-    Light = require "systems/light"
+    Light = require "systems/light",
+    Generator = require "systems/generator",
+    Thruster = require "systems/thruster"
 }
